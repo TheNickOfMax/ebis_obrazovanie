d4501c8c288cbeb45574867479e094a8b0bfbf6c
LaPepega <tmv03092008@gmail.com>
2023-12-28 15:01:55 +0500

    renamed request function

--- src/api.rs
+++ src/api.rs
@@ -3,7 +3,7 @@ use crate::credentials;
 use reqwest::Method;
 
 // make the api request and return the responce text
-pub async fn req() -> Result<String, reqwest::Error> {
+pub async fn request_estimate() -> Result<String, reqwest::Error> {
     let cli: reqwest::Client = reqwest::Client::new();
     let resp: String = cli
         .request(Method::GET, credentials::URL)
