95f2b30749c5b686d4b728f20eb6eaaec7f90062
LaPepega <tmv03092008@gmail.com>
2023-12-28 15:01:55 +0500

    FromJson trait, so it's more convinient to convert to Vecs

---
+++ src/from_json.rs
@@ -0,0 +1,41 @@
+use json::JsonValue;
+
+pub trait FromJson<T> {
+    fn from_json_array(value: JsonValue) -> Vec<T>;
+}
+
+impl FromJson<String> for Vec<String> {
+    fn from_json_array(value: JsonValue) -> Vec<String> {
+        value
+            .members()
+            .map(|s| s.as_str().unwrap_or_default().to_string())
+            .collect::<Vec<String>>()
+    }
+}
+
+impl FromJson<i32> for Vec<i32> {
+    fn from_json_array(value: JsonValue) -> Vec<i32> {
+        value
+            .members()
+            .map(|s| s.as_i32().unwrap_or_default())
+            .collect::<Vec<i32>>()
+    }
+}
+
+impl FromJson<JsonValue> for Vec<JsonValue> {
+    fn from_json_array(value: JsonValue) -> Vec<JsonValue> {
+        value
+            .members()
+            .map(|j| j.to_owned())
+            .collect::<Vec<JsonValue>>()
+    }
+}
+
+impl FromJson<Vec<String>> for Vec<Vec<String>> {
+    fn from_json_array(value: JsonValue) -> Vec<Vec<String>> {
+        value
+            .members()
+            .map(|j| Vec::<String>::from_json_array(j.to_owned()))
+            .collect()
+    }
+}
