78eef08287c7d19fd9cdc43351c3cfc193c45d93
LaPepega <tmv03092008@gmail.com>
2023-12-28 15:01:55 +0500

    Basic request with credentials

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,19 @@
 use std::error::Error;
+
+use reqwest::Method;
+mod cred;
+
+const url: &str = "";
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
+    let cli = reqwest::Client::new();
+    let resp = cli
+        .request(Method::GET, url)
+        .bearer_auth(cred::AUTH)
+        .send()
+        .await?
+        .text()
+        .await?;
+
     return Ok(());
 }
