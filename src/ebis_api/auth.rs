2a05828739990d6fa9846a30a439875a0d75ebe6
LaPepega <tmv03092008@gmail.com>
2023-12-30 01:42:54 +0500

    finished the auth function Now i only need to update all the ebis api requests  to use it

--- src/ebis_api/auth.rs
+++ src/ebis_api/auth.rs
@@ -1,21 +1,35 @@
-// https://dnevnik.egov66.ru/api/auth/swagger/index.html
-// https://dnevnik.egov66.ru/api/auth/broker/esia/login?client_id=aiss2-diary
-
-pub async fn gos_login_token(login: &str, password: &str) -> Result<String, reqwest::Error> {
+/// Logs into the provided gosuslugi acount and gets a user's auth code
+pub async fn gos_login_code(login: &str, password: &str) -> Result<String, reqwest::Error> {
     let login_body = format!("{{\"login\":\"{login}\",\"password\":\"{password}\"}}");
 
     let cli: reqwest::Client = reqwest::Client::builder().cookie_store(true).build()?;
 
-    let req_d = cli
+    // Initial request to dnevnik's login, which redirects to gosuslugi
+    let req_dnev = cli
         .get("https://dnevnik.egov66.ru/api/auth/broker/esia/login?client_id=aiss2-diary")
         .send();
-    let _resp_d = req_d.await?;
+    let _resp_dnev = req_dnev.await?;
 
-    let req_g = cli
+    // Request to the gosuslugi's login, which gives a redirect link back to dnevnik
+    let req_gos = cli
         .post("https://esia.gosuslugi.ru/aas/oauth2/api/login")
         .body(login_body)
         .send();
-    let resp_g = req_g.await?;
+    let resp_gos = req_gos.await?;
 
-    Ok(resp_g.text().await?.to_string())
+    // Parse the auth finishing redirect url
+    let gos_json = json::parse(&resp_gos.text().await?).unwrap();
+    let redir = gos_json["redirect_url"].as_str().unwrap();
+
+    // Auth finishing request to the dnevnik
+    let last = cli.get(redir).send();
+    let last_resp = last.await?;
+
+    // Code to get dnvnik's bearer token is inside the last url
+    let code_url = last_resp.url().as_str().to_string();
+    let code = &code_url[code_url.rfind("code=").unwrap() + 5..]
+        .to_string()
+        .replace("%3d", "=");
+
+    Ok(code.to_string())
 }
