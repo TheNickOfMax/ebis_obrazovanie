437fcca73fefc1d3ae5647b7753581caa98e30ac
LaPepega <tmv03092008@gmail.com>
2023-12-29 17:26:58 +0500

    replaced hashmap with literal body, gosuslugi is still shit and returns the wrong url

--- src/ebis_api/auth.rs
+++ src/ebis_api/auth.rs
@@ -1,16 +1,15 @@
 use std::collections::HashMap;
 
 pub async fn gos_login_token(login: &str, password: &str) -> Result<String, reqwest::Error> {
-    let mut req_body: HashMap<&str, &str> = HashMap::new();
-    req_body.insert("login", login);
-    req_body.insert("password", password);
+    let req_body = format!("{{\"login\":\"{login}\",\"password\":\"{password}\"}}");
 
     let cli: &reqwest::Client = &reqwest::Client::new();
 
     let req = cli
         .post("https://esia.gosuslugi.ru/aas/oauth2/api/login")
-        .json(&req_body)
+        .body(req_body)
         .send();
+
     let resp = req.await?.text().await?;
 
     let redirect_url = json::parse(&resp).unwrap()["redirect_url"]
@@ -18,8 +17,8 @@ pub async fn gos_login_token(login: &str, password: &str) -> Result<String, reqw
         .unwrap_or_default()
         .to_string();
 
-    let token = &redirect_url.as_str()
-        [redirect_url.find("code=").unwrap()..redirect_url.find("&").unwrap()];
+    let token = &redirect_url.as_str()[redirect_url.find("code=").unwrap_or_default()
+        ..redirect_url.find("&").unwrap_or_default()];
 
-    Ok(token.to_string())
+    Ok(resp.to_string())
 }
